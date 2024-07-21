3b6b169674ad24ff274b22c26f3371fb4533f0a6
JulianaYS <u20201b782@upc.edu.pe>
2024-07-21 17:47:38 -0500

    feat: update.

--- landing-page-release1/assets/js/main.js
+++ landing-page-release1/assets/js/main.js
@@ -78,6 +78,21 @@
     }
   });
 
+  new Swiper('.invierte-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
  
    window.addEventListener('load', () => {
     AOS.init({
